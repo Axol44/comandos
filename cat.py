c9ce69e91ba27516bd8bb286657a1bbd140c82af
Sol Huitzil <paquetito@boquipato.es>
2021-01-03 12:35:28 -0600

    Optimizar manejo de archivos con with

--- cat.py
+++ cat.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import sys
 
-f = open(sys.argv[1])
-for linea in f.read():
-    print(linea, end='')
-f.close()
+with open(sys.argv[1]) as f:
+    for linea in f.read():
+        print(linea, end='')
